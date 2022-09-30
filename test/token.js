239c5dcd0e19d40071b77adb33ebfd1edc186721
Hammad Mustafa <tohammad@gmail.com>
2022-09-30 22:07:39 +0530

    hardhat commit

--- test/token.js
+++ test/token.js
@@ -4,15 +4,28 @@ const { ethers } = require('hardhat');
 describe("Token contract", () => {
     it("Deployment should assign the total supply of tokens to the owner", async() => {
         const [owner] = await ethers.getSigners();
-        console.log(`Signers: ${JSON.stringify(owner)}`);
+        //console.log(`Signers: ${JSON.stringify(owner)}`);
 
         const Token = await ethers.getContractFactory("Token")
         const hardhatToken = await Token.deploy();
         const ownerBalance = await hardhatToken.balanceOf(owner.address);
 
-        console.log(`owner address: ${owner.address}`);
+        //console.log(`owner address: ${owner.address}`);
 
         expect (await hardhatToken.totalSupply()).to.equal(ownerBalance);
+
+    }); 
+    it("Should transfer token between accounts", async() => {
+        const [owner, addr1, addr2] = await ethers.getSigners();
+        console.log(`Signers: ${JSON.stringify(owner)}`);
+
+        const Token = await ethers.getContractFactory("Token")
+        const hardhatToken = await Token.deploy();
+        await hardhatToken.transfer(addr1.address, 10);
+        expect (await hardhatToken.balanceOf(addr1.address)).to.equal(10);
+
+        await hardhatToken.connect(addr1).transfer(addr2.address, 5);
+        expect (await hardhatToken.balanceOf(addr2.address)).to.equal(5);
 
     }); 
 })
