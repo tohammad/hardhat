80a5466461125c29227c4c73d7654aeda9cd74e0
Hammad Mustafa <tohammad@gmail.com>
2022-10-03 21:41:01 +0530

    hardhat commit

--- test/token.js
+++ test/token.js
@@ -1,31 +1,26 @@
-const {expect} = require('chai');
-const { ethers } = require('hardhat');
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Token contract", () => {
-    it("Deployment should assign the total supply of tokens to the owner", async() => {
-        const [owner] = await ethers.getSigners();
-        //console.log(`Signers: ${JSON.stringify(owner)}`);
+  let token, hardhatToken, owner, addr1, addr2;
 
-        const Token = await ethers.getContractFactory("Token")
-        const hardhatToken = await Token.deploy();
-        const ownerBalance = await hardhatToken.balanceOf(owner.address);
+  beforeEach( async ()  => {
+    token = await ethers.getContractFactory("Token");
+    [owner, addr1, addr2] = await ethers.getSigners();
+    hardhatToken = await token.deploy();
+  })
+  it("Deployment should assign the total supply of tokens to the owner", async () => {
+    const ownerBalance = await hardhatToken.balanceOf(owner.address);
+    //console.log(`owner address: ${owner.address}`);
+    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+  });
+  it("Should transfer token between accounts", async () => {
+    console.log(`Signers: ${JSON.stringify(owner)}`);
 
-        //console.log(`owner address: ${owner.address}`);
+    await hardhatToken.transfer(addr1.address, 10);
+    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(10);
 
-        expect (await hardhatToken.totalSupply()).to.equal(ownerBalance);
-
-    }); 
-    it("Should transfer token between accounts", async() => {
-        const [owner, addr1, addr2] = await ethers.getSigners();
-        console.log(`Signers: ${JSON.stringify(owner)}`);
-
-        const Token = await ethers.getContractFactory("Token")
-        const hardhatToken = await Token.deploy();
-        await hardhatToken.transfer(addr1.address, 10);
-        expect (await hardhatToken.balanceOf(addr1.address)).to.equal(10);
-
-        await hardhatToken.connect(addr1).transfer(addr2.address, 5);
-        expect (await hardhatToken.balanceOf(addr2.address)).to.equal(5);
-
-    }); 
-})
+    await hardhatToken.connect(addr1).transfer(addr2.address, 5);
+    expect(await hardhatToken.balanceOf(addr2.address)).to.equal(5);
+  });
+});
